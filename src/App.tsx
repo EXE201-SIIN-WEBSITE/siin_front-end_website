aac2ecca813c44af40dcf7c0f4933c52dd15f4f2
Doan Thanh Hieu <hieudoan280102@gmail.com>
2024-05-15 13:38:54 +0700

    project initialization and setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline text-red-500 text-center mb-2 top-[16px]'>Hello world!</h1>
+      <p className='bg-blue-300 text-green-800'>Config done!</p>
+    </>
+  )
+}
+
+export default App
