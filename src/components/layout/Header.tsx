83762a5b4ca9127aa1779bc9316e7c606d0b43fb
Tran Pham Thanh Nam <trannam.nt865@gmail.com>
2024-07-01 22:32:39 +0700

    siderbar

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,7 +6,13 @@ import { RootState, useAppDispatch } from '~/redux/containers/store'
 import { getUserIdByToken, getUserInfo } from '~/redux/actions/user.actions'
 import { logout } from '~/redux/actions/auth.action'
 
-const Header = () => {
+interface HeaderProps {
+  siderbarRef: React.RefObject<HTMLDivElement>
+  setIsSidebarOpen: React.Dispatch<React.SetStateAction<boolean>>
+  isSidebarOpen: boolean
+}
+
+const Header = ({ siderbarRef, setIsSidebarOpen, isSidebarOpen }: HeaderProps) => {
   const [isProductsHovered, setIsProductsHovered] = useState(false)
   const [isDropdownOpen, setIsDropdownOpen] = useState(false)
   const [cartItemCount, setCartItemCount] = useState(0)
@@ -14,6 +20,7 @@ const Header = () => {
   const dispatch = useAppDispatch()
   const [, setLoading] = useState(true)
   const navigate = useNavigate()
+
   const fixedAvatar =
     'https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg?size=338&ext=jpg&ga=GA1.1.2116175301.1719100800&semt=ais_user'
   useEffect(() => {
@@ -128,13 +135,20 @@ const Header = () => {
 
   return (
     <header className='w-full text-white bg-black'>
-      <div className='container flex items-center justify-around mx-auto'>
-        <div className='flex justify-center basis-2/12'>
-          <Logo />
+      <div className='container flex items-center justify-between px-4 mx-auto'>
+        <div className='flex items-center'>
+          <button className='text-white md:hidden' onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+            <i className='fa fa-bars text-[24px] mx-10'></i>
+          </button>
+          <div className='flex items-center justify-center'>
+            <Link to='/'>
+              <Logo />
+            </Link>
+          </div>
         </div>
 
-        <nav className='relative flex items-center justify-end basis-8/12'>
-          <div className='flex items-center w-full justify-evenly'>
+        <nav className={`flex-col items-center justify-end hidden md:flex md:flex-row md:basis-8/12`}>
+          <div className='flex flex-col items-center w-full md:flex-row justify-evenly'>
             <NavLink
               to={'/'}
               className='text-white hover:text-gray-400 size-7 sm:text-[20px] lg:text-[24px] text-nowrap'
@@ -144,7 +158,7 @@ const Header = () => {
             <NavLink
               onMouseEnter={() => setIsProductsHovered(true)}
               onMouseLeave={() => setIsProductsHovered(false)}
-              to={'products'}
+              to={'/products'}
               className='text-white hover:text-gray-400 size-7 hover:scale-105 translate-x-1 sm:text-[20px] lg:text-[24px] text-nowrap relative'
             >
               Sản Phẩm
@@ -160,7 +174,7 @@ const Header = () => {
               )}
             </NavLink>
             <NavLink
-              to={'customize'}
+              to={'/customize'}
               className='text-white hover:text-gray-400 size-7 sm:text-[20px] lg:text-[24px] text-nowrap'
             >
               Customize
@@ -168,8 +182,8 @@ const Header = () => {
           </div>
         </nav>
 
-        <div className='flex gap-4 sm:justify-around lg:justify-evenly basis-2/12'>
-          <NavLink className='relative text-white hover:text-gray-400 sm:text-[20px] text-[24px]' to={'cart'}>
+        <div className='flex justify-between gap-4 md:justify-evenly'>
+          <NavLink className='relative text-white hover:text-gray-400 sm:text-[20px] text-[24px]' to={'/cart'}>
             <i className='fa-solid fa-cart-shopping hover:text-gray-400 sm:text-[20px] text-[24px]'></i>
             {cartItemCount > 0 && (
               <span className='absolute flex items-center justify-center w-5 h-4 text-xs text-center bg-red-500 rounded-full top-5 left-5'>
@@ -179,10 +193,58 @@ const Header = () => {
           </NavLink>
 
           {renderUserLink()}
-          {/* <NavLink className='text-white hover:text-gray-400 sm:text-[20px] text-[24px]' to={'login'}>
-            <i className='fa-solid fa-circle-user'></i> 
-          </NavLink> */}
         </div>
+      </div>
+
+      {/* Sidebar */}
+      <div
+        ref={siderbarRef}
+        className={`fixed top-0 left-0 h-full w-64 bg-black transform ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out z-50`}
+      >
+        <button className='absolute text-white top-4 right-4' onClick={() => setIsSidebarOpen(false)}>
+          <i className='fa fa-times text-[24px]'></i>
+        </button>
+        <nav className='flex flex-col mt-16'>
+          <NavLink
+            to={'/'}
+            className='px-4 py-2 text-xl text-white hover:text-gray-400'
+            onClick={() => setIsSidebarOpen(false)}
+          >
+            Trang chủ
+          </NavLink>
+          <NavLink
+            onMouseEnter={() => setIsProductsHovered(true)}
+            onMouseLeave={() => setIsProductsHovered(false)}
+            to={'/products'}
+            className='relative px-4 py-2 text-xl text-white hover:text-gray-400'
+            onClick={() => setIsSidebarOpen(false)}
+          >
+            Sản Phẩm
+            {isProductsHovered && (
+              <div className='absolute top-0 left-32'>
+                <Link
+                  to='/sell'
+                  className='block px-4 py-2 my-2 text-sm rounded-md bg-zinc-500 hover:bg-white hover:text-black'
+                >
+                  SELL
+                </Link>
+                <Link
+                  to='/buy'
+                  className='block px-4 py-2 text-sm rounded-md hover:bg-white bg-zinc-500 hover:text-black'
+                >
+                  DIY
+                </Link>
+              </div>
+            )}
+          </NavLink>
+          <NavLink
+            to={'/customize'}
+            className='px-4 py-2 text-xl text-white hover:text-gray-400'
+            onClick={() => setIsSidebarOpen(false)}
+          >
+            Customize
+          </NavLink>
+        </nav>
       </div>
     </header>
   )
